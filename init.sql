e5c794521ef2d367dd93baa875a54081cd5e0884
PhongHai <phonghai1901@gmail.com>
2026-03-31 16:16:25 +0700

    new

--- init.sql
+++ init.sql
@@ -1,15 +1,5 @@
 -- tạo user
 CREATE USER todo_user WITH PASSWORD '123456';
-
--- tạo database
-CREATE DATABASE todo;
-
--- cấp quyền
-GRANT ALL PRIVILEGES ON DATABASE todo TO todo_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO todo_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO todo_user;
-
--- connect vào DB
 \c todo
 
 -- tạo table
@@ -17,3 +7,8 @@ CREATE TABLE IF NOT EXISTS todos (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL
 );
+
+-- cấp quyền
+GRANT ALL PRIVILEGES ON DATABASE todo TO todo_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO todo_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO todo_user;
